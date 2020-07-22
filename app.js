07bd715edc2a506022fbdb74d0478faf7c4b8088
Samuel Losada <samthesam26@gmail.com>
2020-07-23 00:14:27 +1000

    added db test query on app.js, need to debug.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 require("dotenv/config");
 
@@ -8,6 +9,17 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log("Connected to DB."));
+
+const u = new User({
+    firstName: 'Samuel',
+    lastName: 'losada',
+    email: "a@b.c",
+    password: '1',
+});
+
+u.save((err) => console.log(err));
+
+console.log(User.find({}));
 
 app.get("/", (req, res) => res.send("Hello World number 2!"));
 
