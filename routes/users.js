637af4897952041b502c97a93496f40f552e49bd
Samuel Losada <samthesam26@gmail.com>
2020-07-27 14:56:21 +1000

    Added update request method for users route. Finish implementing update paramaters.

--- routes/users.js
+++ routes/users.js
@@ -17,13 +17,25 @@ router.post('/', async (req, res) => {
             firstName: req.body.firstName,
             lastName: req.body.lastName, 
             email: req.body.email,
-            password: req.body.password
+            password: req.body.password, 
+            categories: req.body.categories
         });  
         user.save();
         res.json(user);
     } catch(err){
         res.json({message: err});
     }
+});
+
+router.patch('/:userId', async (req, res) => {
+    try{
+        //NEED TO FINISH THIS UPDATE REQUEST.
+        const updatedUser = await User.findByIdAndUpdate({_id: req.params.userId});
+        res.json(updatedUser)
+    } catch(err){
+        res.json({mesage: err});
+    }
+    
 });
 
 router.delete('/:userId', async (req, res) => {
